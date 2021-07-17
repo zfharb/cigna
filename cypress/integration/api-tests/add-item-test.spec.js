b6e539fb063c370cfe77249d989ccfe62ab609b2
Ziad Harb <ziadharb@Ziads-MBP.attlocal.net>
2021-07-17 17:54:46 -0500

    test: modify api test

--- cypress/integration/api-tests/add-item-test.spec.js
+++ cypress/integration/api-tests/add-item-test.spec.js
@@ -4,15 +4,16 @@ describe('test suite for demoblaze site', () => {
       it('it should add samsung galaxy s6 to cart', () => {
         cy.request({
           method: 'POST',
-          url: 'https://api.demoblaze.com/addtocart',
+          url: ' https://api.demoblaze.com/signup',
           body: {
-            "cookie": "user=8efdf1ea-6ff8-146b-73d9-5f41a654ac9a",
-            "flag": "false",
-            "id": "586be94d-7fef-fe1b-016c-b40a9fa507b0",
-            "prod_id": "1"
+            'password': "123",
+            'username': "zztop1234567098123"
           }
         }).then((res) => {
-          expect(res.status).to.eq(200)
+          expect(res.status).to.eq(200);
+          cy.log("zzzzzzzz")
+
+          cy.log(res.body.data)
         })
 
 
